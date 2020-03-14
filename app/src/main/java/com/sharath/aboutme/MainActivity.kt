d37866471ef75e94db7a04acb77b13b94118851d
Sharath <sharathpavan7@gmail.com>
2020-03-14 23:00:35 +0530

    commit - v0.0.1 added some UI elements to the layout.

--- app/src/main/java/com/sharath/aboutme/MainActivity.kt
+++ app/src/main/java/com/sharath/aboutme/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.sharath.aboutme
 
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val doneButton: Button = findViewById(R.id.done_button)
+        doneButton.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        val nicknameEdit: EditText = findViewById(R.id.nickname_edit)
+        val nicknameText: TextView = findViewById(R.id.nickname_text)
+
+        nicknameText.text = nicknameEdit.text
+        nicknameEdit.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        // Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
